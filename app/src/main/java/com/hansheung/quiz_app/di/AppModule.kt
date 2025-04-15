d4c902ac4bec09bd884f98c43cdf0c95f63ccf66
Hansheung <hansheung@gmail.com>
2025-04-15 14:09:43 +0800

    Initializing main

---
+++ app/src/main/java/com/hansheung/quiz_app/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.hansheung.quiz_app.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+
+    @Provides
+    @Singleton
+    @Named("msg0")
+    fun provideGreetingMsg(): String{
+        return "Hello Dagger Hilt 2"
+    }
+
+}
---
+++ app/src/main/java/com/hansheung/quiz_app/ui/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package com.hansheung.quiz_app.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor (
+
+) : ViewModel() {
+    // TODO: Implement the ViewModel
+}
